fa651f8419ac6a6ad118ab2c9f2eefa9ac2a0f34
Kikinomini <nkiwatrowski@web.de>
2018-11-23 17:53:27 +0100

    feat(): Javascript versuch

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-result',
-  templateUrl: './result.component.html',
-  styleUrls: ['./result.component.less']
+    selector: 'app-result',
+    templateUrl: './result.component.html',
+    styleUrls: ['./result.component.less']
 })
 export class ResultComponent implements OnInit {
 
-  constructor() { }
+    public collapsed: boolean;
 
-  ngOnInit() {
-  }
+    constructor() {
+        this.collapsed = false;
+        this.collapse();
+        console.log(this.collapsed);
+    }
+
+    ngOnInit() {
+    }
+
+    public collapse() {
+        if (this.collapsed === true) {
+            this.collapsed = false;
+        } else {
+            this.collapsed = true;
+        }
+        console.log(this.collapsed);
+
+    }
 
 }
